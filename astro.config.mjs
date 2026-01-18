b791e9e40fd9d23a53905d5c5f1e1ff2647360b0
makolodz <maximekolodz@gmail.com>
2026-01-18 10:35:26 +0100

    static deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import vue from "@astrojs/vue";
 
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   integrations: [
     icon(),
     vue()
